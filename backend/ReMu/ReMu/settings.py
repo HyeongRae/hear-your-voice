f86827e1b47847202c13bad9c5833a0db595ea52
HyeongRae <gudfo19950@naver.com>
2020-11-06 19:11:38 +0900

    Update settings.py

--- backend/ReMu/ReMu/settings.py
+++ backend/ReMu/ReMu/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j&6b1iqdr4l@ad@c3&qd56h8xxijs8e18dr#g0ee@qx5ezm0j!'
+
+#SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
